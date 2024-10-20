0b4064f0e1dbe8cfc7e932d840b761208568fb94
Piyush Prajapati <140145139+piyush0213@users.noreply.github.com>
2024-10-20 15:01:10 +0530

    Delete Projects/drishtii.js

--- Projects/drishtii.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const menuContainer = document.getElementById('menu-container');
-    // Toggle the visibility of the menu container
-    if (menuContainer.style.display === 'block') {
-        menuContainer.style.display = 'none';
-    } else {
-        menuContainer.style.display = 'block';
-    }
-}
